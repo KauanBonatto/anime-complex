fd793e5ef86eff654c97e1c198b677b61073dee9
Kauan Bonatto <76924039+KauanBonatto@users.noreply.github.com>
2024-07-04 23:10:41 -0300

    fix: Api, creating local instance of api

--- src/services/ApiService/index.ts
+++ src/services/ApiService/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const apiService = axios.create({
-  baseURL: 'https://api.consumet.org/anime/gogoanime'
+    baseURL: 'https://consumet-api-eta-three.vercel.app/anime/gogoanime'
+  // baseURL: 'https://api.consumet.org/anime/gogoanime'
 });
