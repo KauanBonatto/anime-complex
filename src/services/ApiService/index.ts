a80ba7136316c2a03966e72749d9d67c3240850f
Kauan Bonatto <76924039+KauanBonatto@users.noreply.github.com>
2025-05-31 11:54:41 -0300

    Update index.ts

--- src/services/ApiService/index.ts
+++ src/services/ApiService/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const apiService = axios.create({
-    baseURL: 'https://consumet-api-eta-three.vercel.app/anime/gogoanime'
-  // baseURL: 'https://api.consumet.org/anime/gogoanime'
+    baseURL: 'https://api-consumet-org-lyart.vercel.app/anime/gogoanime'
+    // baseURL: 'https://consumet-api-eta-three.vercel.app/anime/gogoanime'
+    // baseURL: 'https://api.consumet.org/anime/gogoanime'
 });
